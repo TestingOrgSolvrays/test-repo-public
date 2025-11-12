bfd48689c4027f0490a4c2ad9f5dfcbb9458f51b
Aayush Shrestha <aayush@solvrays.com>
2025-11-12 19:08:12 +0545

    modified index

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-app.get("/", (req, res)=>{
-	res.send("Hello from test");
+app.use(cors());
+app.use(express.json());
+
+
+// Dummy user
+const USER = { email: "test@example.com", password: "123456" };
+
+
+app.post("/login", (req, res) => {
+const { email, password } = req.body;
+
+
+if (email === USER.email && password === USER.password) {
+return res.json({ success: true, message: "Login successful" });
+}
+
+
+res.json({ success: false, message: "Invalid email or password" });
 });
 
-const port = 8888;
 
-app.listen(port, ()=>{console.log("App is running");});
-
+app.listen(5000, () => console.log("Backend running on http://localhost:5000"));
